9e0a34752bd62a10ef941328616e7eec8d789969
Hoshimotao <jthomas302@gmail.com>
2020-01-12 02:28:09 -0500

    remove x's

--- TicTacToe/scripts.js
+++ TicTacToe/scripts.js
@@ -1,8 +1,15 @@
- 
-  const player1 = "X"
-  const player2 = "O"
+$(document).ready(function(){
+  let player1 = "X"
+  let player2 = "O"
 
-  console.log("players are ready")
+  let turn = 1;
+  let movesMade = 0
+
+  let box = $(".box");
+  box.on("click", function(e){    console.log(e, "Suh dude")
+  })
+
+})
 
  
 
